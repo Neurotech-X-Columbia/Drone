e82bd26a4b4a4dba5526206368c81007cd0ccca0
matheucampell <matheu.campbell.us@outlook.com>
2022-12-04 18:03:38 -0500

    simulated headset added

--- main_pipeline/PipelineClasses.py
+++ main_pipeline/PipelineClasses.py
@@ -1,9 +1,13 @@
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from os import path
 from time import sleep
+
+import numpy as np
 
 
 class Headset:
     def __init__(self, window_size, srate, serial_port, board_id=2):
+        """Class to handle starting stream, ending stream, and collecting data"""
         self.serial_port = serial_port
         self.bid = board_id
         self.buf_size = window_size + 1
@@ -17,6 +21,7 @@ class Headset:
             raise BufferSizeException("Headset buffer size must be larger than chunksize.")
 
     def initialize(self):
+        """Prepare OpenBCI  for streaming"""
         params = BrainFlowInputParams()
         params.serial_port = self.serial_port
         self.board = BoardShim(self.bid, params)
@@ -25,6 +30,7 @@ class Headset:
         self.is_active = True
 
     def start_stream(self):
+        """Start reading data into buffer"""
         if not self.is_active:
             print("Cannot start stream before initializing headset.")
             return
@@ -37,22 +43,70 @@ class Headset:
         self.is_active = True
 
     def end_stream(self):
+        """End stream and cleanup"""
         self.board.stop_stream()
         self.board.release_session()
         self.is_active = False
 
     def collect(self):
+        """Return data currently in the buffer without removing it from the buffer"""
         return self.board.get_current_board_data()[self.drows[0]: self.drows[-1]+1]
 
 
+class SimulatedHeadset:
+    def __init__(self, window_size, overlap, datapath):
+        """Class to simulate live capture with prerecorded dataset
+           window_size: number of samples to process at once (same as chunk_size)
+           overlap: how much of the last chunk to include in new chunk as a proportion
+           datapath: path to sample data"""
+        pathway = datapath
+        cwd = path.dirname(path.dirname(path.abspath(__file__)))
+        filepath = path.join(cwd, pathway)
+        self.data = np.genfromtxt(filepath, delimiter=';', skip_header=True, usecols=(1, 2)).transpose()
+
+        self.chunk_size = window_size  # Chunk size in samples
+        self.overlap = overlap  # Percent of old chunk to include in new chunk
+        self.stream = self.chunk_generator()
+        self.is_active = True
+
+    def chunk_generator(self):
+        """Generator that returns 'newest' chunk"""
+        shift = int(self.overlap*self.chunk_size)
+        for start in range(0, len(self.data[0]), shift):
+            end = min(len(self.data[0]), start+self.chunk_size)
+
+            yield self.data[:, start:end]
+
+    def collect(self):
+        """Returns next chunk if possible, otherwise returns 0 array"""
+        try:
+            return next(self.stream)
+        except StopIteration:
+            print("Stream ended (processed all data)")
+            self.is_active = False
+            return np.zeros((1, 1))
+
+    def end_stream(self):
+        self.is_active = False
+
+
 class Processor:
-    def __init__(self, states, default_state, *proc_funcs):
+    def __init__(self, states, default_state, *proc_funcs, logfile=None):
+        """Class to handle keeping track of current state, running processing functions on new chunks, and updating
+        the current state"""
         self.states = states
         self.current = default_state
         self.proc_funcs = proc_funcs
         self.state_dict = {}
 
+        if logfile:  # Will log the result of analysis on every chunk
+            self.log = open(logfile, 'w')
+            self.logging = True
+        else:
+            self.logging = False
+
     def process_chunk(self, data):
+        """Runs every processing function on data"""
         for state in self.states:
             self.state_dict[state] = 0
 
@@ -62,13 +116,23 @@ class Processor:
         self.update_state()
 
     def update_state(self):
+        """Implements majority rules to determine new state"""
         state_counts = list(zip(self.states, [self.state_dict[st] for st in self.states]))
         state_counts.sort(key=lambda x: x[1])
 
         self.current = state_counts[-1][0]
 
+    def log_change(self, start, end, new_state, srate=250):
+        """Saves new state in a log file"""
+        self.log.write(f"[{round(start/srate, 7)}s ({start} samples) \t {round(end/srate, 7)}s ({end} samples)]\n")
+        self.log.write(f"{new_state}\n")
+
     def get_state(self):
         return self.current
+
+    def close_log(self):
+        if self.logging:
+            self.log.close()
 
 
 class BufferSizeException(Exception):
--- main_pipeline/realtime_parallel.py
+++ main_pipeline/realtime_parallel.py
@@ -1,58 +1,77 @@
 import mne
 import numpy as np
-from PipelineClasses import Headset, Processor
-from signal_processing_scripts.DataClasses import Container
+
+from time import ctime
+from PipelineClasses import Headset, SimulatedHeadset, Processor
 
 # import random
 
 
 def process_loop(proc, *hs_params):
-    headset = Headset(*hs_params)
-    headset.initialize()
-    headset.start_stream()
+    # headset = Headset(*hs_params)
+    # headset.initialize()
+    # headset.start_stream()
 
+    headset = SimulatedHeadset(*hs_params)
+
+    chunk_num = 0
     while headset.is_active:
         try:
-            proc.process_chunk(headset.collect())
-            print(f"State: {proc.get_state()}")
+            if (chunk := headset.collect()).any():
+                proc.process_chunk(chunk)
+                chunk_num += 1
+
+            new = proc.get_state()
+            start = int(chunk_num * (overlap*window_size))
+            end = start + window_size
+            proc.log_change(start, end, new_state=new)
+
+            print(f"State: {new}")
+
         except KeyboardInterrupt:
             headset.end_stream()
             print("Session end.")
 
-    # active = True
-    # while active:
-    #     try:
-    #         new_chunk = random.sample(range(0, 100), 50)
-    #         proc.process_chunk(new_chunk)
-    #         print(f"State: {proc.get_state()}")
-    #     except KeyboardInterrupt:
-    #         active = False
-    #         print("Session end.")
+    proc.close_log()
 
 # Change to custom processing functions that take in only data as parameters and return a state as output
 
 
-def detect_blinks(data):
+def detect_blinks(data):  # Accepts data and returns the number of blinks detected in it
     chan_names = ['FP1', 'FP2']
-    sample_freq = 125
+    sample_freq = 250
     lpass = 5
     hpass = 55
     mne_info = mne.create_info(chan_names, sample_freq, 'eeg')
 
     raw_mne = mne.io.RawArray(data, info=mne_info)
     raw_mne.filter(l_freq=lpass, h_freq=hpass)
-    threshold = np.max(raw_mne[0][0]) - np.min(raw_mne[0][0]) / 4
-    eog_events = mne.preprocessing.find_eog_events(raw_mne, ch_name=["One", "Two"], thresh=threshold)
+    threshold = (np.max(raw_mne[0][0]) - np.min(raw_mne[0][0])) / 4
+    eog_events = mne.preprocessing.find_eog_events(raw_mne, ch_name=["FP1", "FP2"], thresh=threshold,
+                                                   filter_length='3s')
     locations = eog_events[:, 0] / srate
 
-    return len(locations)
+    if len(locations):
+        state = 'blink'
+    else:
+        state = 'no blink'
+
+    return state
 
 
 if __name__ == '__main__':
-    window_size = 500
+    # For both live and simulated
+    window_size = 375
+
+    # For live data collection
     srate = 125
     serial_port = 'COM3'
     board_id = 2
+
+    # For simulated data collection
+    overlap = .3
+    datapath = "signal_processing_scripts\\Src\\EEG-EyeBlinks\\EEG-IO\\S06_data.csv"
+    logfilename = f"log.txt"
 
     states = ['blink', 'no blink']
     default = 'no blink'
@@ -60,5 +79,8 @@ if __name__ == '__main__':
     # Tuple of processing functions to apply to every chunk. Should accept data and return state
     funcs = (detect_blinks)
 
-    pro = Processor(states, default, funcs)
-    process_loop(pro, *(window_size, srate, serial_port, board_id))
+    pro = Processor(states, default, funcs, logfile=logfilename)
+
+    hs_params = (window_size, srate, serial_port, board_id)
+    shs_params = (window_size, overlap, datapath)
+    process_loop(pro, *shs_params)
